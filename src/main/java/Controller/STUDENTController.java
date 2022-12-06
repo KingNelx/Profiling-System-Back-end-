88a9de6239d120c71b6049d12dc63cb133153d5f
KingNelx <tapiajonel606@gmail.com>
2022-12-06 12:14:52 +0800

    added exception fixed custom query

--- src/main/java/Controller/STUDENTController.java
+++ src/main/java/Controller/STUDENTController.java
@@ -30,5 +30,75 @@ public class STUDENTController {
         return studentRepo.findAll();
     }
 
+    // bsit1
+    @GetMapping("/getBSIT1")
+    List<String> getAllBSIT1(){
+        return studentRepo.getAllBSIT1();
+    }
 
+    // bsit2
+    @GetMapping("/getBSIT2")
+    List<String> getAllBSIT2(){
+        return studentRepo.getAllBSIT2();
+    }
+
+    // bsit3
+    @GetMapping("/getBSIT3")
+    List<String> getAllBSIT3(){
+        return studentRepo.getAllBSIT3();
+    }
+
+    // bsit4
+    @GetMapping("/getBSIT4")
+    List<String> getAllBSIT4(){
+        return studentRepo.getAllBSIT4();
+    }
+
+    // bsis1
+    @GetMapping("/getBSIS1")
+    List<String> getAllBSIS1(){
+        return studentRepo.getAllBSIS1();
+    }
+
+    // bsis2
+    @GetMapping("/getBSIS2")
+    List<String> getAllBSIS2(){
+        return studentRepo.getAllBSIS2();
+    }
+
+    // bsis3
+    @GetMapping("/getBSIS3")
+    List<String> getAllBSIS3(){
+        return studentRepo.getAllBSIS3();
+    }
+
+    // bsis4
+    @GetMapping("/getBSIS4")
+    List<String> getAllBSIS4(){
+        return studentRepo.getAllBSIS4();
+    }
+
+    // bsc1
+    @GetMapping("/getBSCS1")
+    List<String> getAllBSCS1(){
+        return studentRepo.getAllBSCS1();
+    }
+
+    // bsc2
+    @GetMapping("/getBSCS2")
+    List<String> getAllBSCS2(){
+        return studentRepo.getAllBSCS2();
+    }
+
+    // bsc3
+    @GetMapping("/getBSCS3")
+    List<String> getAllBSCS3(){
+        return studentRepo.getAllBSCS3();
+    }
+
+    // bsc4
+    @GetMapping("/getBSCS4")
+    List<String> getAllBSCS4(){
+        return studentRepo.getAllBSCS4();
+    }
 }
